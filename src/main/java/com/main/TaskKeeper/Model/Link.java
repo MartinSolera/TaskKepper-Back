e34a9c3f4d6645ed9e4d82fc67a8facb245f23d7
Martin <martinsolera71@gmail.com>
2023-08-15 11:21:44 -0300

    act git

--- src/main/java/com/main/TaskKeeper/Model/Link.java
+++ src/main/java/com/main/TaskKeeper/Model/Link.java
@@ -1,4 +1,24 @@
 package com.main.TaskKeeper.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "link")
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Link {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
 }
+
+
--- src/main/java/com/main/TaskKeeper/Util/ListarPDF.java
+++ src/main/java/com/main/TaskKeeper/Util/ListarPDF.java
@@ -47,3 +47,5 @@ public class ListarPDF extends AbstractPdfView {
     }
 
 }
+
+///add
